92902d6741acabecfdadf2ca3bfd5a90c79c3329
y0za <yoza.is12s@gmail.com>
2018-07-20 01:34:42 +0900

    Add model.PredeclaredType tests

---
+++ model/model_test.go
@@ -0,0 +1,57 @@
+package model
+
+import "testing"
+
+func TestPredeclaredTypeString(t *testing.T) {
+	cases := []struct {
+		pType    PredeclaredType
+		expected string
+	}{
+		{
+			PredeclaredType("string"),
+			"string",
+		},
+		{
+			PredeclaredType("int"),
+			"int",
+		},
+		{
+			PredeclaredType("bool"),
+			"bool",
+		},
+		{
+			PredeclaredType("float64"),
+			"float64",
+		},
+		{
+			PredeclaredType("error"),
+			"error",
+		},
+	}
+
+	pt := PackageTable{}
+	for _, tt := range cases {
+		actual := tt.pType.String(pt)
+		if actual != tt.expected {
+			t.Errorf(`expected "%s" actual "%s"`, tt.expected, actual)
+		}
+	}
+}
+
+func TestPredeclaredTypeAddPackagePaths(t *testing.T) {
+	cases := []PredeclaredType{
+		PredeclaredType("string"),
+		PredeclaredType("int"),
+		PredeclaredType("bool"),
+		PredeclaredType("float64"),
+		PredeclaredType("error"),
+	}
+
+	for _, pt := range cases {
+		pps := PackagePathSet{}
+		pt.addPackagePaths(pps)
+		if len(pps) != 0 {
+			t.Errorf("expected PackagePathSet is empty actual %#v", pps)
+		}
+	}
+}
